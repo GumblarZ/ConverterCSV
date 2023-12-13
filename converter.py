a0e10f12cc8a761fccf2a2b2659a038512bce52b
gumblarz <gumblarz@outlook.com>
2023-12-13 11:35:01 -0300

    adicionado tkinter para selecionar o arquivo

--- converter.py
+++ converter.py
@@ -1,6 +1,9 @@
 import pandas
 
-file = #file CSV#
+def CSV(file):
+    csv = pandas.read_csv(file)
+    return csv.to_excel('new.xlsx',index = None, header = True)
 
-csv = pandas.read_csv(file)
-csv.to_excel('new.xlsx',index = None, header = True)
+def Excel(file):
+    Excel = pandas.read_excel(file)
+    return Excel.to_csv('new.csv')
