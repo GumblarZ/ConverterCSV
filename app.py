5b12d9ac43d90c5ef942bf0905b6498170ccfc4b
José Paiva <gumblarz@outlook.com>
2025-02-15 10:08:34 -0300

    melhorando o tratamento

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ file = fd.askopenfilename()
 
 name = os.path.splitext(file)
 
-if(name[1] == '.xlsx'):
-    Convert.EXCEL(file, name[0])
+if(name[1] == '.xlsx'): Convert.EXCEL(file, name[0])
 
-if(name[1] == '.csv'):
-    Convert.CSV(file, name[0])
+elif(name[1] == '.csv'): Convert.CSV(file, name[0])
+
+else: print('Arquivo não suportado')
+
